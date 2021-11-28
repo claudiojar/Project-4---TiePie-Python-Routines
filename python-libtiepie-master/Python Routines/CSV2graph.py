70e224ce783cd9863612736bbe495790e408f7a9
Claudio Jaramilllo <claudio.jaramilloconcha@epfl.ch>
2021-11-28 16:06:02 +0100

    Added CSV2Graph

--- python-libtiepie-master/Python Routines/CSV2graph.py
+++ python-libtiepie-master/Python Routines/CSV2graph.py
@@ -14,12 +14,50 @@ from MiraexLib.printinfo import*
 
 import csv
 import pandas
+import matplotlib.pyplot as plt
 
 
 # %% Settings
 name = '12KferruleH70e'
 selected_file = miraex_misc.OpenFileDialog()
 
-with open(selected_file, 'r') as csv_file:
+# We have to specify the header list names, because in the CSV file generated the actual coplumn names are commented out with the # character.
+# Ideally we would create a CSV file where the header name line is NOT a comment, thus remove the initial # char
+header_list = ['timestamp', 'rms ch1', 'std ch1',
+               'rms ch2', 'std ch2', 'sled power', 'sled power std']
 
-    print(selected_file)
+my_data = pandas.read_csv(selected_file, engine='python',
+                          sep=',', encoding='utf-8', comment='#', names=header_list)
+
+# %% Plotting
+
+plot_titles = ['Channel 1', 'Channel 2']
+my_xlabel = 'Time'
+my_global_title = 'RMS Voltage per Channel'
+
+yData = [[my_data['rms ch1'][i] for i in range(len(my_data))],
+         [my_data['rms ch2'][i] for i in range(len(my_data))]]
+xData = [my_data['timestamp'][i] for i in range(len(my_data))]
+
+# Define plot elements
+my_fig, ax = plt.subplots(nrows=2, ncols=1)
+
+# define how many ticks in the x axis we want to show
+nb_x_ticks = 5
+nb_x_ticks += 1
+
+for nn, ax in enumerate(ax):
+    # actual lines plotted
+    ax.plot(xData, yData[nn], '-')
+
+    ax.set_title(f'{plot_titles[nn]}')
+    ax.grid(which='major')
+    # ax.legend()
+    start, end = ax.get_xlim()
+    ax.xaxis.set_ticks(np.arange(start, end, (end-start)/nb_x_ticks))
+    ax.set_ylabel('RMS Voltage')
+
+
+ax.set_xlabel(my_xlabel)
+my_fig.supxlabel(my_global_title, fontsize=20)
+plt.show()
