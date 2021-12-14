40904fade018afed8edbb691cda0ead3257d552e
Claudio Jaramilllo <claudio.jaramilloconcha@epfl.ch>
2021-12-14 18:14:30 +0100

    Small modifications

--- python-libtiepie-master/Python Routines/FiberCharacterisationJUG.py
+++ python-libtiepie-master/Python Routines/FiberCharacterisationJUG.py
@@ -11,6 +11,8 @@ from __future__ import print_function
 import libtiepie
 import time
 import matplotlib.pyplot as plt
+import os
+import sys
 
 import numpy as np
 import MiraexLib.plot as miraex_plt
@@ -77,12 +79,17 @@ This should solve the ''No backend available'' exepction raised at init of the c
 Warning : use the set_max_voltage method before the set_output_voltage method to avoid crashes.
 """
 
-# %% Parameters
+# %% Generic parameters
 
 name = 'DM-0001-DC1_on_top '
 
+# %% Cuboid parameters
+
+V_max = 75  # 75 V maximum voltage
+V_default = 20  # V default voltage
+
 # %% Settings ref perfect mirror
-refCh = [2.340, 1.123]  # Signal with perfect mirror in V [Ch1 Ch2]
+refCh = [2.340, 1.123]  # Signal with perfect mirror in V [Ch1 Ch2], reference
 
 # %% Settings for oscilloscope
 Lrec = 10000  # Recording length
@@ -107,31 +114,32 @@ for sn in cuboids:
     cubini.set_input_mode()
     cubinis.append(cubini)
     print('KPZ101 {} found.'.format(sn))
+
 """
     except Exception as e:
         print(e)
         print('KPZ101 {} not found.'.format(sn))
 """
-
 """
 # sequentially run a test sequence for each cube
 for kpz in cubinis:
-    kpz.set_max_voltage(150)
+    kpz.set_max_voltage(V_max)
     kpz.enable_output()
     kpz.set_output_voltage(0)
     time.sleep(0.5)
-    for v in np.linspace(0, 50, 50):
+    for v in np.linspace(0, 50, 25):
         kpz.set_output_voltage(v)
         time.sleep(0.1)
     kpz.set_output_voltage(0)
     kpz.disable_output()
 """
 
+
 # set initial parameters sequentially
 for kpz in cubinis:
-    kpz.set_max_voltage(150)
+    kpz.set_max_voltage(V_max)
     kpz.enable_output()
-    kpz.set_output_voltage(20)
+    kpz.set_output_voltage(V_default)
 
 
 # %% TiePie initialization
@@ -153,7 +161,7 @@ Vch = []  # Channel signal vector
 # Set acquire
 samplerate = 1
 bufferdelay = 0.1
-df = (1.25*10**6)
+df = (1.25*(10**6))
 
 dt = 1/df
 
@@ -161,10 +169,149 @@ iitotal = 20
 
 # %% Main loop
 
-for ii in range(1, 1, 1):
+for ii in range(1, 1, 1):  # start, stop, step
     trigger = 0
     voltstop = 0
     hystTest = np.arange(3, 65, 1)
 
     for tt in hystTest:
         print('hello world')
+
+        # Try to open an oscilloscope with block measurement support and a generator in the same device:
+        scp = None
+        gen = None
+        for item in libtiepie.device_list:
+            if (item.can_open(libtiepie.DEVICETYPE_OSCILLOSCOPE)) and (item.can_open(libtiepie.DEVICETYPE_GENERATOR)):
+                scp = item.open_oscilloscope()
+                if scp.measure_modes & libtiepie.MM_BLOCK:
+                    gen = item.open_generator()
+                    break
+                else:
+                    scp = None
+
+        if scp and gen:
+            try:
+                # Oscilloscope settings:
+
+                # Set measure mode:
+                scp.measure_mode = libtiepie.MM_BLOCK
+
+                # Set sample frequency:
+                scp.sample_frequency = Fs  # Hz
+
+                # Set record length:
+                scp.record_length = Lrec  # nb of samples
+
+                # Set pre sample ratio:
+                scp.pre_sample_ratio = 0  # 0 %
+
+                # For all channels:
+                for ch in scp.channels:
+                    # Enable channel to measure it:
+                    ch.enabled = True
+
+                    # Set range:
+                    ch.range = ampRange  # V
+
+                    # Set coupling:
+                    ch.coupling = libtiepie.CK_DCV  # DC Volt
+
+                # Set trigger timeout:
+                scp.trigger_time_out = 1  # 1 s
+
+                # Disable all channel trigger sources:
+                for ch in scp.channels:
+                    ch.trigger.enabled = False
+
+                # Locate trigger input:
+                # or TIID_GENERATOR_START or TIID_GENERATOR_STOP
+                trigger_input = scp.trigger_inputs.get_by_id(
+                    libtiepie.TIID_GENERATOR_NEW_PERIOD)
+
+                if trigger_input is None:
+                    raise Exception('Unknown trigger input!')
+
+                # Enable trigger input:
+                trigger_input.enabled = True
+
+                # Generator settings:
+
+                # Set signal type:
+                gen.signal_type = libtiepie.ST_SINE
+
+                # Set frequency:
+                gen.frequency = 1e3  # 1 kHz
+
+                # Set amplitude:
+                gen.amplitude = 2  # 2 V
+
+                # Set offset:
+                gen.offset = 0  # 0 V
+
+                # Enable output:
+                gen.output_on = True
+
+                # Print oscilloscope info:
+                print_device_info(scp)
+
+                # Print generator info:
+                print_device_info(gen)
+
+                # Start measurement:
+                scp.start()
+
+                # Start signal generation:
+                gen.start()
+
+                # Wait for measurement to complete:
+                while not scp.is_data_ready:
+                    time.sleep(0.01)  # 10 ms delay, to save CPU time
+
+                # Stop generator:
+                gen.stop()
+
+                # Disable output:
+                gen.output_on = False
+
+                # Get data:
+                data = scp.get_data()
+
+                # Output CSV data:
+                csv_file = open('OscilloscopeGeneratorTrigger.csv', 'w')
+                try:
+                    csv_file.write('Sample')
+                    for i in range(len(data)):
+                        csv_file.write(';Ch' + str(i + 1))
+                    csv_file.write(os.linesep)
+                    for i in range(len(data[0])):
+                        csv_file.write(str(i))
+                        for j in range(len(data)):
+                            csv_file.write(';' + str(data[j][i]))
+                        csv_file.write(os.linesep)
+
+                    print()
+                    print('Data written to: ' + csv_file.name)
+
+                finally:
+                    csv_file.close()
+            except Exception as e:
+                print('Exception: ' + e.message)
+                sys.exit(1)
+
+            # Close oscilloscope:
+            del scp
+
+            # Close generator:
+            del gen
+
+        else:
+            print('No oscilloscope available with block measurement support or generator available in the same unit!')
+            sys.exit(1)
+
+        sys.exit(0)
+
+# Reset cuboids to default parameters
+for kpz in cubinis:
+    kpz.set_max_voltage(V_max)
+    kpz.enable_output()
+    kpz.set_output_voltage(V_default)
